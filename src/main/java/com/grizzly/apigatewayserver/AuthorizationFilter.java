dbce9a4a4443e113ac3c05561a9d41b84ff6640c
Jordan <jordan.sakellariou@gmail.com>
2018-10-10 13:27:06 +1100

    Fixing header removal

---
+++ src/main/java/com/grizzly/apigatewayserver/AuthorizationFilter.java
@@ -0,0 +1,38 @@
+package com.grizzly.apigatewayserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class AuthorizationFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "route";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 5;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        String tokenId = request.getHeader("Authorization");
+        if (tokenId != null) {
+            ctx.addZuulRequestHeader("Authorization", tokenId);
+        }
+
+        return null;
+    }
+}
